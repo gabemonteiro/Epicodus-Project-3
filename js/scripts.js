46a5243cacdc13c400ab6cc1e2c2fa0e614fb8e7
unknown <gabe.c.monteiro@gmail.com>
2019-10-06 21:25:58 -0700

    correct formatting for priority

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,25 @@ $(document).ready(function() {
     for (var i = 0; i < number + 1; i += 1) {
       var value = i.toString();
 
-    if (value.indexOf('1') !== -1) {
-      output.push('Beep!')
+      if (value.indexOf('3') !== -1) {
+        output.push("I'm sorry, Dave. I'm afraid I can't do that.")
+        continue
+      }
+
+      if (value.indexOf('2') !== -1) {
+        output.push('Boop!')
+        continue
+      }
+
+      if (value.indexOf('1') !== -1) {
+        output.push('Beep!')
+        continue
+      }
+
+    output.push(i)
     }
 
-    if (value.indexOf('2') !== -1) {
-      output.push('Boop!')
-    }
-
-    if (value.indexOf('3') !== -1) {
-      output.push("I'm sorry, Dave. I'm afraid I can't do that.")
-    }
+    var result = output.join(', ')
 
     $('#result').text(result)
     event.preventDefault()
